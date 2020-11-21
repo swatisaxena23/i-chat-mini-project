1fea93050b33dc25bb0a2262842a89784990364b
swatisaxena <swati.saxena_cs18@gla.ac.in>
2020-11-22 01:52:27 +0530

    css done

--- js/client.js
+++ js/client.js
@@ -1,33 +1,42 @@
-const e = require("express");
-
-const socket = io('http://localhost:2300')
+const socket = io('http://localhost:8000');
 
 const form = document.getElementById('send-container');
-const messageInput = document.getElementById('messageInp');
-const messageContainer = document.querySelector('.container');
-const append = (message , position)=>{
+const messageInput = document.getElementById('messageInp')
+const messageContainer = document.querySelector('.container')
+
+
+var audio_receive = new Audio('ting.mp3');
+
+const append = (message, position) => {
     const messageElement = document.createElement('div');
     messageElement.innerText = message;
     messageElement.classList.add('message');
     messageElement.classList.add(position);
     messageContainer.append(messageElement);
+    if (position == 'left')
+        audio_receive.play();
+   
 }
-form.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    const message=messageInput.nodeValue;
-    append(`You: ${message}`)
 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You : ${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value = ''
 })
 
-const name = prompt('Enter Your Name to Join');
-
+const name = prompt("Enter Your Name To Join The Chat");
 socket.emit('new-user-joined', name);
 
-socket.on('user-joined',name=>{
-append(`${name} joined the chat`, 'right');
+socket.on('user-joined', name => {
+    append(`${name} has joined the chat...!`, 'right')
 })
 
-socket.on('receive',data=>{
-    append(`${data.message}: ${data.user}`, 'left');
-    })
+socket.on('receive', data => {
+    append(`${data.name} :${data.message}`, 'left')
+})
+socket.on('left', name => {
+    append(`${name} left the chat`, 'left')
+})
 
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,13 +1,24 @@
-// node Server which will handle socket.io 
-const io = require('socket.io')(2300)
-const users = {};
-io.on('connection', socket =>{
-    socket.on('new-user-joined',name=>{
-        users[socket.id]= name;
-        socket.broadcast.emit('user-joined', name)
-    });
-socket.on('send', message=>{
-    socket.broadcast.emit('receive', {message: message, name: user[socket.id]})
-});
 
-})
+//Node server whick will handel Socket IO connections
+
+const io = require('socket.io')(8000)
+
+const users = {};
+
+io.on('connection', socket => {
+    socket.on('new-user-joined', name => {
+        //console.log("new-user", name)
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', { message: message, name: users[socket.id] })
+    });
+
+    socket.on('disconnect', message => {
+        socket.broadcast.emit('left', users[socket.id])
+        delete users[socket.id];
+    });
+
+});
