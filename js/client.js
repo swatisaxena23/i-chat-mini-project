722b6d96b128839e48ac0c1000bda3219f2bae41
swatisaxena <swati.saxena_cs18@gla.ac.in>
2020-11-21 23:53:36 +0530

    hello

--- js/client.js
+++ js/client.js
@@ -1,6 +1,8 @@
-const socket = io('http://localhost:8000')
+const e = require("express");
 
-const form = document.getElementById('send-container') ;
+const socket = io('http://localhost:2300')
+
+const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
 const messageContainer = document.querySelector('.container');
 const append = (message , position)=>{
@@ -10,10 +12,22 @@ const append = (message , position)=>{
     messageElement.classList.add(position);
     messageContainer.append(messageElement);
 }
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const message=messageInput.nodeValue;
+    append(`You: ${message}`)
 
-const name = prompt('enter your name');
+})
+
+const name = prompt('Enter Your Name to Join');
 
 socket.emit('new-user-joined', name);
-socket.on('user-joined',data =>{
+
+socket.on('user-joined',name=>{
 append(`${name} joined the chat`, 'right');
 })
+
+socket.on('receive',data=>{
+    append(`${data.message}: ${data.user}`, 'left');
+    })
+
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,13 +1,13 @@
 // node Server which will handle socket.io 
-const io = require('socket.io')(8000)
+const io = require('socket.io')(2300)
 const users = {};
 io.on('connection', socket =>{
     socket.on('new-user-joined',name=>{
         users[socket.id]= name;
-        socketIO.broadcast.emit('user-joined', name)
+        socket.broadcast.emit('user-joined', name)
     });
 socket.on('send', message=>{
-    socket.broadcast.emit('recieve', {message: message, name: user[socket.id]})
+    socket.broadcast.emit('receive', {message: message, name: user[socket.id]})
 });
 
 })
